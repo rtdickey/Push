5acca2ec942d53c4c5798dd6bad729732673cbd4
Stefan Theard <smtheard@gmail.com>
2016-02-21 21:06:06 -0600

    board updated in ui, basic style in

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,15 +1,21 @@
-var createFriendlyTable = function(playerSocket){
+var createTable = function(playerSocket){
 	for(var i = 0; i < 8; i++) {
       var tr = document.createElement("tr");
       for(var j = 0; j < 4; j++) {
-        var button = document.createElement("BUTTON");
-        button.id = i + ',' + j;
-        //button.onclick = placeShipHandler(i, j, playerSocket);
-        var td = document.createElement("td");
-        var txtBtn = document.createTextNode(" _____ ");
-        button.appendChild(txtBtn);
-        td.appendChild(button);
-        tr.appendChild(td);
+        var cell = document.createElement("td");
+        if(!(i===0&&j===0 || i===0&&j===3 ||
+         i===1&&j===3 || i===6&&j===0 ||
+         i===7&&j===0 || i===7&&j===3)){
+          var cell = document.createElement("td");
+          cell.id = i + ',' + j;
+          cell.innerHTML = i + ',' + j;
+          //cell.onclick = placeShipHandler(i, j, playerSocket);
+          tr.appendChild(cell);
+        }else{
+          var blank = document.createElement("td")
+          blank.id = "blank"
+          tr.appendChild(blank);
+        }
       }
       document.getElementById('boardArea').appendChild(tr);
     }
