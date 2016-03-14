5fa1dd6ad9d24489bccb21f3e8fd77cef43278a4
Stefan Theard <smtheard@gmail.com>
2016-03-13 23:21:21 -0500

    include socket io add reminder for setting up player socket

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var config = require('config');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
