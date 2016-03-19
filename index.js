4afa2136c6a45eb7ad266680af0e4c84ab635e73
Stefan Theard <smtheard@gmail.com>
2016-03-19 00:31:17 -0500

    initial test socket in, first route in as well. getting weird errors atm though

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ app.listen(config.port, function() {
 
     console.log('Express static server running on ' + environment + ' ' + port);
 });
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+})
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
